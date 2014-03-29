8be13842c01c4c29a81c87a43c0fb19649581a64
shimpeiws <shimpeiws@gmail.com>
2014-03-29 16:38:29 +0900

    add Home

--- web.rb
+++ web.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require "sinatra/json"
+require 'erb'
 get '/' do
-  'Hello world!(restart)'
+  erb :index
 end
 
 get '/param/:id' do
     "param test [id = #{params[:id]}]"
 end
+
+get '/api' do
+  data = { foo: "bar" }
+  json data
+end
+
